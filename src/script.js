7c99f1d8832d1e46ba533393e52beb5c75d33967
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2023-12-18 10:40:25 -0800

    add code sign in

--- src/script.js
+++ src/script.js
@@ -1,9 +1,20 @@
-var body = document.querySelector("body")
-var color1 = document.getElementById ("color1")
-var color2 = document.getElementById("color2")
-var showcolor1 = document.getElementById("showcolor1")
-var showcolor2 = document.getElementById("showcolor2")
-var pre = document.getElementById("showCode");
+const body = document.querySelector("body")
+const color1 = document.getElementById ("color1")
+const color2 = document.getElementById("color2")
+const showcolor1 = document.getElementById("showcolor1")
+const  showcolor2 = document.getElementById("showcolor2")
+const pre = document.getElementById("showCode");
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const rpassword = document.getElementById('r-password')
+const siginin = document.getElementById('sigin-in')
+const button = document.getElementById('b')
+const div = document.getElementById('div2')
+const div2 = document.getElementById('div3')
+const divp = document.querySelector(".p1")
+const divp2 = document.querySelector(".p2")
+const divp3 = document.querySelector(".p3")
+const divn = document.querySelector(".background2")
 function action(){
     body.style.background =
     "linear-gradient(to right ,"+ color1.value +","+ color2.value+")";
@@ -18,6 +29,42 @@ color2.addEventListener("input" , action)
 
 
 function copy_text(){
-    navigator.clipboard.writeText(color1.value+","+color2.value);
-    alert('متن کپی شد');
+    if(button.style.display != "flex"){
+        divn.innerHTML = `<p style = "color:red;">!اول وارد شوید</p>`
+    }
+    else{
+        navigator.clipboard.writeText(color1.value+","+color2.value);
+        divp.innerHTML += `<p>${color1.value + color2.value}</p>`
+        divp2.innerHTML += `<p>${color1.value + color2.value}</p>`
+        divp3.innerHTML += `<p>${color1.value + color2.value}</p>`
+        divn.innerHTML = ""
+        
+    }
+}
+
+function sigin(){
+    div.style.top = "100px"
+}
+
+function submit(){
+    if(email.value.length == 0 && password.value.length == 0 && rpassword.value.length == 0){
+       div2.innerHTML = `<p style="color:red;">!لطفا اطلاعات خواسته شده را وارد کنید</p>`
+    }
+    else if(email.value.length <8){
+        div2.innerHTML = `<p style="color:red;">!ایمیل خالی است</p>`
+    }
+    else if(password.value.length <8){
+        div2.innerHTML = `<p style="color:red;">!رمز عبور خالی است</p>`
+    }
+    else if(rpassword.value.length <8){
+        div2.innerHTML = `<p style="color:red;">!رمز عبور را تکرار کنید</p>`
+    }
+    else if(password.value != rpassword.value){
+        div2.innerHTML = `<p style="color:red;">!رمز عبور با تکرارش مطابقت ندارد</p>`
+    }
+    else{
+    div.style.top = "-600px"
+    button.style.display = "flex"
+    siginin.style.display = "none"
+    }
 }
