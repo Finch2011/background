d6bd1842e8edfae0b0c755153250b4d48d8000cf
mohamad <130075161+mohamadzeza@users.noreply.github.com>
2023-12-20 12:03:51 -0800

    new style

--- src/script.js
+++ src/script.js
@@ -1,20 +1,35 @@
 const body = document.querySelector("body")
+//color input
 const color1 = document.getElementById ("color1")
 const color2 = document.getElementById("color2")
+//
+// show color in body
 const showcolor1 = document.getElementById("showcolor1")
 const  showcolor2 = document.getElementById("showcolor2")
+//
+//show code color in p
 const pre = document.getElementById("showCode");
+//
+//div sign in 
+const div = document.getElementById('div2')
+//a place for ereror sign in
+const div2 = document.getElementById('div3')
+//
+//inputs for sgin in
 const email = document.getElementById('email')
 const password = document.getElementById('password')
 const rpassword = document.getElementById('r-password')
+//
+//Element for sign in
 const siginin = document.getElementById('sigin-in')
 const button = document.getElementById('b')
-const div = document.getElementById('div2')
-const div2 = document.getElementById('div3')
-const divp = document.querySelector(".p1")
-const divp2 = document.querySelector(".p2")
-const divp3 = document.querySelector(".p3")
+const eye = document.getElementById('eye')
+const eye2 = document.getElementById('eye2')
+//
+// set by querySelector  erorr for first sign in
 const divn = document.querySelector(".background2")
+//
+// change color by input
 function action(){
     body.style.background =
     "linear-gradient(to right ,"+ color1.value +","+ color2.value+")";
@@ -27,25 +42,24 @@ function action(){
 color1.addEventListener("input" , action)
 color2.addEventListener("input" , action)
 
-
+//
+// error for sign in by p
 function copy_text(){
     if(button.style.display != "flex"){
         divn.innerHTML = `<p style = "color:red;">!اول وارد شوید</p>`
     }
     else{
         navigator.clipboard.writeText(color1.value+","+color2.value);
-        divp.innerHTML += `<p>${color1.value + color2.value}</p>`
-        divp2.innerHTML += `<p>${color1.value + color2.value}</p>`
-        divp3.innerHTML += `<p>${color1.value + color2.value}</p>`
-        divn.innerHTML = ""
-        
+        alert("کپی شد")
     }
 }
-
+//
+// code a div for sign in 
 function sigin(){
     div.style.top = "100px"
 }
-
+//
+// Code a div for login errors
 function submit(){
     if(email.value.length == 0 && password.value.length == 0 && rpassword.value.length == 0){
        div2.innerHTML = `<p style="color:red;">!لطفا اطلاعات خواسته شده را وارد کنید</p>`
@@ -66,5 +80,31 @@ function submit(){
     div.style.top = "-600px"
     button.style.display = "flex"
     siginin.style.display = "none"
+    
     }
 }
+//
+//code a img for type password
+function eyeShow() {
+    if(password.type == "password"){
+        eye.src="./src/eye.svg"
+        password.type = "text"
+    }
+    
+   else if(password.type == "text"){
+        eye.src="./src/eye-slash.svg"
+        password.type = "password"
+    }
+}
+function eyeShow2() {
+    if(rpassword.type == "password"){
+        eye2.src="./src/eye.svg"
+        rpassword.type = "text"
+    }
+    
+   else if(rpassword.type == "text"){
+        eye2.src="./src/eye-slash.svg"
+        rpassword.type = "password"
+    }
+}
+//
